b4dcb6b40240717270f7a20f0742c1305c85935a
Jon Packer <contact@jonpacker.com>
2013-06-08 18:33:07 +0200

    Tests passing

---
+++ format-measurement.js
@@ -0,0 +1,35 @@
+function guessPrecision(number) {
+  var parts = String(number).split('.')
+  if (parts.length == 1) return 0;
+  else return parts[1].length > 2 ? 2 : parts[1].length;
+}
+
+function formatNumber(number) {
+  return number.toFixed(guessPrecision(number));
+}
+
+var metricWeight = {
+  '1000': function(num) { return formatNumber(num / 1000) + ' kg' },
+  '0': function(num) { return formatNumber(num) + ' g' }
+};
+
+var metricVolume = {
+  '1000': function(num) { return formatNumber(num / 1000) + ' L' },
+  '0': function(num) { return formatNumber(num) + ' mL' }
+};
+
+function createFormatter(formatters) {
+  var breakpoints = Object.keys(formatters).map(parseFloat).sort().reverse();
+  console.log(breakpoints)
+  return function(num) {
+    var i = 0;
+    var formatter = breakpoints[i];
+    while (formatter > num) formatter = breakpoints[++i];
+    return formatters[formatter](num);
+  };
+};
+
+exports.metric = {
+  weight: createFormatter(metricWeight),
+  volume: createFormatter(metricVolume)
+};
--- test/test.js
+++ test/test.js
@@ -3,23 +3,23 @@ var assert = require('assert');
 
 describe('Weight formatter', function() {
   it ('Should format kg', function() {
-    assert(fmt.weight.metric(1000) == '1 kg');
-    assert(fmt.weight.metric(10000) == '10 kg');
+    assert.equal(fmt.metric.weight(1000), '1 kg');
+    assert(fmt.metric.weight(10000) == '10 kg');
   });
 
   it ('Should format grams', function() {
-    assert(fmt.weight.metric(100) == '100 g');
-    assert(fmt.weight.metric(10) == '10 g');
+    assert(fmt.metric.weight(100) == '100 g');
+    assert(fmt.metric.weight(10) == '10 g');
   });
 
   it ('Should format fractions', function() {
-    assert(fmt.weight.metric(12.56) == '12.56 g');
+    assert(fmt.metric.weight(12.56) == '12.56 g');
   });
 
   it ('Should guess precision', function() {
-    assert(fmt.weight.metric(12) == '12 g');
-    assert(fmt.weight.metric(15.4) == '15.4 g');
-    assert(fmt.weight.metric(15.64) == '16.64 g');
-    assert(fmt.weight.metric(16.625) == '16.63 g');
+    assert(fmt.metric.weight(12) == '12 g');
+    assert(fmt.metric.weight(15.4) == '15.4 g');
+    assert.equal(fmt.metric.weight(15.64), '15.64 g');
+    assert.equal(fmt.metric.weight(16.625), '16.63 g');
   });
 });
